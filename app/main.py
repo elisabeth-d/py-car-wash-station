aa60abe79f11c738786f3eed3ccaeed07d9405cb
elisabeth-d <elizavetadudka39@gmail.com>
2026-01-21 16:31:45 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 profit_from_washing += self.calculate_washing_price(car)
-                self.wash_one_car(car)
+                self.wash_single_car(car)
         return round(profit_from_washing, 1)
     def calculate_washing_price(self, car):
         price_of_washing = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating/ self.distance_from_city_center)
@@ -22,7 +22,7 @@ class CarWashStation:
     def wash_single_car(self, car):
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
-    def rate_service(self, sum_of_voting):
-        sum_of_voting = self.average_rating * self.count_of_ratings + sum_of_voting
+    def rate_service(self, new_rating):
+        new_rating = self.average_rating * self.count_of_ratings + new_rating
         self.count_of_ratings += 1
-        self.average_rating = round (sum_of_voting / self.count_of_ratings, 1)
+        self.average_rating = round (new_rating / self.count_of_ratings, 1)
